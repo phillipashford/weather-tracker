85163ca747d656c22346e984101274379d0f0ae4
Phillip  Ashford <phillip.ashford@uky.edu>
2023-02-08 11:57:25 -0500

    Format comments

--- weather_tracker.js
+++ weather_tracker.js
@@ -1,20 +1,14 @@
-////////////////////////////////////////////////////////////////////////////////
 // Imports
-////////////////////////////////////////////////////////////////////////////////
 import { weatherApiKey } from "./config.js";
 
-////////////////////////////////////////////////////////////////////////////////
 // Capture user input
-////////////////////////////////////////////////////////////////////////////////
 var valid_input = false
 
 while (valid_input == false) {
 
     let loc_format = prompt('Enter 1 to use geographic coordinates for your query. Enter 2 to use a zipcode for your query. ')
 
-    ////////////////////////////////////////////////////////////////////////////////
     // Determine location info format
-    ////////////////////////////////////////////////////////////////////////////////
     if (loc_format == "1") {
         
         valid_input = true;
@@ -23,9 +17,7 @@ while (valid_input == false) {
         var lat = prompt('Please enter your latitude. ');
         var lon = prompt('Please enter your longitude. ');
 
-        ////////////////////////////////////////////////////////////////////////////////
         // Access and return current weather conditions to user
-        ////////////////////////////////////////////////////////////////////////////////
         $.ajax({
           type: 'GET',
           url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`,
@@ -37,9 +29,7 @@ while (valid_input == false) {
           }
         });
 
-        ////////////////////////////////////////////////////////////////////////////////
         // Access and return 5 day forecast to user
-        ////////////////////////////////////////////////////////////////////////////////
         $.ajax({
           type: 'GET',
           url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`,
@@ -61,21 +51,16 @@ while (valid_input == false) {
         var zipcode = prompt('Please enter your zipcode. ');
         var country_code = prompt('Please enter your country code. ');
 
-        ////////////////////////////////////////////////////////////////////////////////
         // Request geo coords based on provided zipcode
-        ////////////////////////////////////////////////////////////////////////////////        
         $.ajax({
           type: 'GET',
           url: `https://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${country_code}&appid=${weatherApiKey}`,
-          // dataType: "jsonp",
           success: function(data) {
             
             var lat = data.coord.lat;
             var lon = data.coord.lon;
 
-            ////////////////////////////////////////////////////////////////////////////////
             // Access and return current weather conditions to user
-            ////////////////////////////////////////////////////////////////////////////////
             $.ajax({
               type: 'GET',
               url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`,
@@ -87,9 +72,7 @@ while (valid_input == false) {
               }
             });
     
-            ////////////////////////////////////////////////////////////////////////////////
             // Access and return 5 day forecast to user
-            //////////////////////////////////////////////////////////////////////////////// 
             $.ajax({
               type: 'GET',
               url: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${weatherApiKey}`,
@@ -111,12 +94,10 @@ while (valid_input == false) {
 
 }
 
-////////////////////////////////////////////////////////////////////////////////
 // Adding the Leaflet Map
-////////////////////////////////////////////////////////////////////////////////
 var map = L.map('map').setView([lat, lon], 7);
 
-///////////Base Layer///////////
+//Base Layer
 L.tileLayer(`https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}`, {
               attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
               maxZoom: 16
@@ -124,7 +105,7 @@ L.tileLayer(`https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_L
 
 const openWeatherLayers = ["clouds_new", "precipitation_new", "temp_new"];          
 
-///////////Openweather Layer Template///////////
+///Openweather Layer Template
 L.tileLayer(`https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${weatherApiKey}`, {
               maxZoom: 19,
               attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
