213d0aa87a8dc8b363c5b55a6c08579bc2952f99
Phillip  Ashford <phillip.ashford@uky.edu>
2023-02-08 22:08:45 -0500

    Refactor code and change GET requests to Axios

--- weather_tracker.js
+++ weather_tracker.js
@@ -1,113 +1,94 @@
 // Imports
+
 import { weatherApiKey } from "./config.js";
 
-// Openweather API Endpoints
-  // [0] Current weather
-  // [1] 5 day forecast
-  // [2] Geocoding
-  // [3] Map layers
-openweatherEndpoints = [
-  `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`,
-  `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`, 
-  `https://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${country_code}&appid=${weatherApiKey}`, 
-  `https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${weatherApiKey}`
-
-];
+var lat;
+var lon;
 
 // Capture user input
 var valid_input = false
 
 while (valid_input == false) {
 
-    let loc_format = prompt('Enter 1 to use geographic coordinates for your query. Enter 2 to use a zipcode for your query. ')
+  let loc_format = prompt('Enter 1 to use geographic coordinates for your query. Enter 2 to use a zipcode for your query. ');
 
-    // Determine location info format
-    if (loc_format == "1") {
-        
-        valid_input = true;
-
-        // Search by geographic coordinates
-        var lat = prompt('Please enter your latitude. ');
-        var lon = prompt('Please enter your longitude. ');
-
-        // Access and return current weather conditions to user
-        $.ajax({
-          type: 'GET',
-          url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`,
-          success: function(data) {
-            console.log(`Current Conditions | ${data.weather[0].description}`);
-          },
-          error: function(error) {
-            console.error(error);
+  // Determine location info format
+  if (loc_format == "1") {
+      
+      valid_input = true;
+  
+      // Search by geographic coordinates
+      var lat = prompt('Please enter your latitude. ');
+      var lon = prompt('Please enter your longitude. ');
+  
+  } else if (loc_format == "2") {
+  
+      valid_input = true;
+  
+      // Search by Zipcode
+      var zipcode = prompt('Please enter your zipcode. ');
+      var country_code = prompt('Please enter your country code. ');
+      let url = `https://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${country_code}&appid=${weatherApiKey}`;
+  
+      axios.get('http://127.0.0.1:3000/proxy', {
+          params: {
+              url: url
           }
-        });
-
-        // Access and return 5 day forecast to user
-        $.ajax({
-          type: 'GET',
-          url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`,
-          success: function(data) {
-            for (var i = 0; i < data.list.length; i++) {
-              console.log(`${data.list[i].dt_txt} | ${data.list[i].weather[0].description} with a temperature of ${data.list[i].main.temp} degrees Fahrenheit.`);
-            };
-          },
-          error: function(error) {
-            console.error(error);
-          }
-        });
-
-    } else if (loc_format == "2") {
-
-        valid_input = true;
-
-        // Search by Zipcode
-        var zipcode = prompt('Please enter your zipcode. ');
-        var country_code = prompt('Please enter your country code. ');
-
-        // Request geo coords based on provided zipcode
-        $.ajax({
-          type: 'GET',
-          url: `https://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${country_code}&appid=${weatherApiKey}`,
-          success: function(data) {
+      })
+      .then(function (response) {
+          var lat = response.data.coord.lat;
+          var lon = response.data.coord.lon;
+      })
+      .catch(function (error) {
+          console.error(error);
+      });
             
-            var lat = data.coord.lat;
-            var lon = data.coord.lon;
+  }  
 
-            // Access and return current weather conditions to user
-            $.ajax({
-              type: 'GET',
-              url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`,
-              success: function(data) {
-                console.log(data.weather[0].description);
-              },
-              error: function(error) {
-                console.error(error);
-              }
-            });
-    
-            // Access and return 5 day forecast to user
-            $.ajax({
-              type: 'GET',
-              url: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${weatherApiKey}`,
-              success: function(data) {
-                console.log(data.weather[0].description);
-              },
-              error: function(error) {
-                console.error(error);
-              }
-            });
-    
-          },
-          error: function(error) {
-            console.error(error);
+} 
+
+getWeather();
+getForecast();
+getMapLayers();
+
+async function getWeather() {
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`;
+
+  try {
+      const response = await axios.get('http://127.0.0.1:3000/proxy', {
+          params: {
+              url: url
           }
-        });
-              
-    }
-
+      });
+      console.log(response.data);
+      console.log(`Current Conditions | ${response.data.weather[0].description}`);
+  } catch (error) {
+      console.error(error);
+      console.log(error.message);
+  }
+  
 }
 
-// Adding the Leaflet Map
+async function getForecast() {
+  try {
+      const response = await axios.get('http://127.0.0.1:3000/proxy', {
+          params: {
+              url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`
+          }
+      });
+      for (var i = 0; i < response.data.list.length; i++) {
+          console.log(`${response.data.list[i].dt_txt} | ${response.data.list[i].weather[0].description} with a temperature of ${response.data.list[i].main.temp} degrees Fahrenheit.`);
+      }
+  } catch (error) {
+      console.error(error);
+      console.log(error.message);
+  }
+}
+
+
+function getMapLayers() {
+  // Adding the Leaflet Map
 var map = L.map('map').setView([lat, lon], 7);
 
 //Base Layer
@@ -124,3 +105,4 @@ L.tileLayer(`https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=
               attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             }).addTo(map);
 
+}
