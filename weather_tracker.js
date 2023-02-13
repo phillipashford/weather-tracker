fe11c2e2136ab9b7214e6352eee925cefd133430
Phillip  Ashford <phillip.ashford@uky.edu>
2023-02-13 07:11:11 -0500

    Change GET requests from Axios to vanilla JS

--- weather_tracker.js
+++ weather_tracker.js
@@ -11,31 +11,31 @@ var lon;
 // Adding the Leaflet Map
 var map = L.map('map').setView([0, 0], 7);
 
+// Capture user input
+var query = prompt("Please enter your location");
+
 //Base Layer
 L.tileLayer(`https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}`, {
             attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
             maxZoom: 16
             }).addTo(map);
 
-// Capture user input
-var query = prompt("Please enter your location");
 
-axios.get('http://127.0.0.1:3000/proxy', {
-    params: {
-        url: `https://nominatim.openstreetmap.org/?addressdetails=1&q=${query}&format=json&limit=1`
-    }
+fetch('http://127.0.0.1:3000/proxy?url=' + encodeURIComponent(`https://nominatim.openstreetmap.org/?addressdetails=1&q=${query}&format=json&limit=1`))
+.then(function(response) {
+  return response.json();
 })
-.then(function (response) {
-    var lat = response.data[0].lat;
-    var lon = response.data[0].lon;
+.then(function(data) {
+  var lat = data[0].lat;
+  var lon = data[0].lon;
 
-    map.setView([lat, lon], 7);
+  map.setView([lat, lon], 7);
 
-    getWeather(lat, lon);
-    getForecast(lat, lon);
+  getWeather(lat, lon);
+  getForecast(lat, lon);
 })
-.catch(function (error) {
-    console.error(error);
+.catch(function(error) {
+  console.error(error);
 });
 
 
@@ -44,29 +44,28 @@ async function getWeather(lat, lon) {
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`;
 
   try {
-      const response = await axios.get('http://127.0.0.1:3000/proxy', {
-          params: {
-              url: url
-          }
-      });
-      console.log(response.data);
-      console.log(`Current Conditions | ${response.data.weather[0].description}`);
-  } catch (error) {x
-      console.error(error);
-      console.log(error.message);
+        const response = await fetch('http://127.0.0.1:3000/proxy?url=' + encodeURIComponent(url));
+        const data = await response.json();
+
+        console.log(data);
+        console.log(`Current Conditions | ${data.weather[0].description}`);
+  } catch (error) {
+        console.error(error);
+        console.log(error.message);
   }
   
 }
 
 async function getForecast(lat, lon) {
-  try {
-      const response = await axios.get('http://127.0.0.1:3000/proxy', {
-          params: {
-              url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`
-          }
-      });
-      for (var i = 0; i < response.data.list.length; i++) {
-          console.log(`${response.data.list[i].dt_txt} | ${response.data.list[i].weather[0].description} with a temperature of ${response.data.list[i].main.temp} degrees Fahrenheit.`);
+  
+  let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`
+  
+    try {
+        const response = await fetch('http://127.0.0.1:3000/proxy?url=' + encodeURIComponent(url));
+        const data = await response.json();
+
+        for (var i = 0; i < data.list.length; i++) {
+            console.log(`${data.list[i].dt_txt} | ${data.list[i].weather[0].description} with a temperature of ${data.list[i].main.temp} degrees Fahrenheit.`);
       }
   } catch (error) {
       console.error(error);
