ea9ea3a591512570d90a82dedef03892ef8fc82f
Phillip  Ashford <phillip.ashford@uky.edu>
2023-02-25 15:34:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,27 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
+const app = express();
 require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+const PORT = 3000;
 
-app.use(cors());
-app.use(express.json());
-
-// Route for getting coordinates based on location
-// app.get('/api/coordinates', async (req, res) => {
-//   try {
-//     const { location } = req.query;
-//     const url = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${process.env.OPENWEATHER_API_KEY}`;
-//     const response = await axios.get(url);
-//     const { lat, lon } = response.data[0];
-//     res.json({ lat, lon });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Server error' });
-//   }
-// });
-
-// // Route for getting weather data based on coordinates
-// app.get('/api/weather', async (req, res) => {
-//   try {
-//     const { lat, lon } = req.query;
-//     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`;
-//     const response = await axios.get(url);
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Server error' });
-//   }
-// });
-
-// Route for getting coordinates based on location
-app.get('/api/coordinates', async (req, res) => {
+app.get('/query', async (req, res) => {
   try {
-    const { location } = req.query;
-    const url = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${process.env.OPENWEATHER_API_KEY}`;
+    const query = req.query.query;
+    const url = `https://nominatim.openstreetmap.org/?addressdetails=1&q=${query}&format=json&limit=1`;
     const response = await axios.get(url);
-    const { lat, lon } = response.data[0];
+    const data = response.data;
+    const lat = data[0].lat;
+    const lon = data[0].lon;
     const weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_API_KEY}&units=imperial`;
+    // // Make the API call to OpenWeatherMap using axios
     const weatherResponse = await axios.get(weatherUrl);
-    res.json({ lat, lon, weather: weatherResponse.data });
+    const weatherData = weatherResponse.data;
+    res.send(weatherData);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).send('Internal server error');
   }
 });
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
