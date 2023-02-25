f420ff84fb3217069480190f96b75930adb740f0
Phillip  Ashford <phillip.ashford@uky.edu>
2023-02-25 15:34:05 -0500

    Update weatherTracker.js

--- weatherTracker.js
+++ weatherTracker.js
@@ -1,4 +1,4 @@
-import { weatherApiKey } from "./config.js";
+// import { weatherApiKey } from "./config.js";
 
 // Defining variables for latitude and longitude
 var lat;
@@ -8,7 +8,7 @@ var lon;
 var map = L.map('mapid').setView([0, 0], 7);
 
 // Capturing user input as a location
-var query = prompt("Please enter your location");
+const query = prompt("Please enter your location");
 
 // Adding the base layer to the map using the ArcGIS service
 L.tileLayer(`https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}`, {
@@ -16,68 +16,27 @@ L.tileLayer(`https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_L
     maxZoom: 16
 }).addTo(map);
 
-// Making an API request to retrieve coordinates from the user's location query
-fetch('http://127.0.0.1:3000/proxy?url=' + encodeURIComponent(`https://nominatim.openstreetmap.org/?addressdetails=1&q=${query}&format=json&limit=1`))
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        var lat = data[0].lat;
-        var lon = data[0].lon;
+getWeatherData();
+
+// Async function to retrieve the weather based on the user's query
+async function getWeatherData() {
+
+    try {
+
+        const response = await fetch(`http://127.0.0.1:3000/query?query=${query}`);
+
+        const data = await response.json();
+        var lat = data.lat;
+        var lon = data.lon;
         
         // Setting the map view to the retrieved latitude and longitude
-        map.setView([lat, lon], 7);
-
-        // Getting the current weather and forecast for the location
-        getWeather(lat, lon);
-        getForecast(lat, lon);
-    })
-    .catch(function (error) {
-        console.error(error);
-    });
-
-// Async function to retrieve the current weather from the OpenWeatherMap API
-async function getWeather(lat, lon) {
-
-    // Defining the URL for the API request
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`;
-
-    try {
-        const response = await fetch('http://127.0.0.1:3000/proxy?url=' + encodeURIComponent(url));
-        const data = await response.json();
-
-        // Logging the weather data and a description of the current conditions
-        console.log(data);
-        console.log(`Current Conditions | ${data.weather[0].description}`);
-
-        // Getting current weather elements in the HTML document
-        var button = document.getElementsByTagName("button");
-
+        map.setView([lat, lon], 6);
 
     } catch (error) {
         console.error(error);
         console.log(error.message);
     }
 
-}
-
-// Async function to retrieve the weather forecast from the OpenWeatherMap API
-async function getForecast(lat, lon) {
-
-    let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`
-
-    try {
-        const response = await fetch('http://127.0.0.1:3000/proxy?url=' + encodeURIComponent(url));
-        const data = await response.json();
-
-        // Looping through the list of forecast data and logging the date, time, weather description, and temperature
-        for (var i = 0; i < data.list.length; i++) {
-            console.log(`${data.list[i].dt_txt} | ${data.list[i].weather[0].description} with a temperature of ${data.list[i].main.temp} degrees Fahrenheit.`);
-        }
-    } catch (error) {
-        console.error(error);
-        console.log(error.message);
-    }
 }
 
 // Array of layer types for the map
@@ -99,7 +58,7 @@ for (var i = 0; i < layerType.length; i++) {
     mapLayers[i].layerName = `${layerType[i]}_new`;
 
     // Adding the tile layer for each layer using the OpenWeatherMap API
-    mapLayers[i].tile = L.tileLayer(`https://tile.openweathermap.org/map/${mapLayers[i]["layerName"]}/{z}/{x}/{y}.png?appid=${weatherApiKey}`, {
+    mapLayers[i].tile = L.tileLayer(`https://tile.openweathermap.org/map/${mapLayers[i]["layerName"]}/{z}/{x}/{y}.png?appid=a855b57546ba54f91cb99e2a482a91eb`, {
         maxZoom: 19,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
